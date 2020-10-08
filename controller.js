11f3353df8af921ac5e6b99ecfefbce5b823ceb4
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2020-10-08 16:59:48 +0200

    moved auth functions to controller.js

--- controller.js
+++ controller.js
@@ -3,6 +3,12 @@
 // Import authentication middleware
 const passport = require('passport');
 
+// Import module for authentication with local strategy (username and password)
+const LocalStrategy = require('passport-local');
+
+// Define constructor for user id
+const ObjectID = require('mongodb').ObjectID;
+
 // Import module for hashing password
 const bcrypt = require('bcrypt');
 
@@ -85,3 +91,39 @@ function ensureAuthenticated(req, res, next) {
   // ...or redirects to home page if user is not
   res.redirect('/');
 }
+
+// Define how to authenticate someone locally (at login or register)...
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    // ...try to find the user in the database...
+    console.log('passport.use =>');
+    client.db('database').collection('users').findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      // ...compare the passwords...
+      if (!bcrypt.compareSync(password, user.password)) {
+        // ...don't pass if the password don't match...
+        return done(null, false);
+      }
+      // ...or pass the user through passport.serializeUser if the password match
+      return done(null, user);
+    });
+  }
+));
+
+// Saves _id from the user object in the session (at register or login)...
+passport.serializeUser((user, done) => {
+  // ...then passes back to passport.authenticate (at register) or passport.session finds _id in the session (request is authenticated) and invokes passport.deserializeUser below (at login)...
+  console.log('passport.serializeUser =>');
+  done(null, user._id);
+});
+
+// ...user object for _id saved in the session is attached to the request on every request (register, login or logout)...
+passport.deserializeUser((id, done) => {
+  console.log('passport.deserializeUser =>');
+  client.db('database').collection('users').findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    if (err) return console.error(err);
+    // ...redirects to profile page through ensureAuthenticated (at register or login) or straight to the home page (at logout)
+    done(null, doc);
+  });
+});
